65a1a15f05756c1d3ca51adaef33a52bb4ec4ca7
RileyDMK <rileydmathis@gmail.com>
2017-10-28 17:44:00 -0700

    feat: Add todo count to render

--- src/App.js
+++ src/App.js
@@ -35,7 +35,9 @@ class App extends Component {
 
   render() {
     return (
-      
+      <div className="container">
+        <h4>Todo Count: <span className="badge badge-primary">{this.state.todos.length}</span></h4>
+      </div>
     );
   }
 }
